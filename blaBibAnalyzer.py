cc049477e691a04471abe2430bf62995a9c9aad4
pedrocastellucci <pedrocastellucci@github.com>
2016-08-16 18:33:39 -0300

    Adds license

--- blaBibAnalyzer.py
+++ blaBibAnalyzer.py
@@ -1,5 +1,19 @@
 # Copyright (c) 2015 Pedro Belin Castellucci
 
+'''
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License version 3 as
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
+
 import matplotlib.pyplot as plt
 import networkx as nx
 import numpy as np
@@ -8,7 +22,7 @@ import sys
 
 def printSplash():
     print ('=============== BlaBibAnalyzer ===============')
-    print ('==Copyright (c) 2015 Pedro Belin Castellucci==')
+    print ('= Copyright (c) 2015 Pedro Belin Castellucci =')
     print ('==============================================')
 
 
